bcfb69ed766118411ce101a06a78f901190aabb0
lehuyhunghedspi <lehuyhunghedspi@gmail.com>
2019-10-21 10:30:53 +0700

    fix

--- src/lib/datasets/dataset/coco.py
+++ src/lib/datasets/dataset/coco.py
@@ -78,6 +78,7 @@ class COCO(data.Dataset):
 
     print('==> initializing coco 2017 {} data.'.format(split))
     self.coco = coco.COCO(self.annot_path)
+    print(self.annot_path)
     print(type(self.coco.dataset['images']))
     print(type(self.coco.dataset['annotations']))
     print(type(self.coco.dataset['categories']))
