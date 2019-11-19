ff3a6876fd0ead2234163bbece1b7dae574b2009
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-11-19 16:26:01 +0700

    a

--- src/demo.py
+++ src/demo.py
@@ -49,7 +49,7 @@ def demo(opt):
     for (image_name) in image_names:
       ret = detector.run(image_name)
       print([value for i,value in enumerate(ret['results'][1][:,4]) if value>0.7])
-      print(np.array([return['results'][1][i][:4] for i,value in enumerate(ret['results'][1][:,4]) if value>0.7]))
+      print(np.array([ret['results'][1][i][:4] for i,value in enumerate(ret['results'][1][:,4]) if value>0.7]))
       time_str = ''
       for stat in time_stats:
         time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
