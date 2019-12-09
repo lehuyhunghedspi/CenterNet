45923973bafb20979e1d22955f04c6f3d6d68b91
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-09 11:41:23 +0700

    fix

--- src/lib/models/networks/pose_dla_dcn.py
+++ src/lib/models/networks/pose_dla_dcn.py
@@ -227,24 +227,26 @@ class DLA(nn.Module):
         super(DLA, self).__init__()
         self.channels = channels
         self.num_classes = num_classes
+
         self.base_layer = nn.Sequential(
             nn.Conv2d(3, channels[0], kernel_size=7, stride=1,
                       padding=3, bias=False),
             nn.BatchNorm2d(channels[0], momentum=BN_MOMENTUM),
             nn.ReLU(inplace=True))
+
+
+        #channels=[16, 32, 64, 128, 256, 512]
+        #levels=[1, 1, 1, 2, 2, 1]
         self.level0 = self._make_conv_level(
             channels[0], channels[0], levels[0])
+
         self.level1 = self._make_conv_level(
             channels[0], channels[1], levels[1], stride=2)
-        self.level2 = Tree(levels[2], block, channels[1], channels[2], 2,
-                           level_root=False,
-                           root_residual=residual_root)
-        self.level3 = Tree(levels[3], block, channels[2], channels[3], 2,
-                           level_root=True, root_residual=residual_root)
-        self.level4 = Tree(levels[4], block, channels[3], channels[4], 2,
-                           level_root=True, root_residual=residual_root)
-        self.level5 = Tree(levels[5], block, channels[4], channels[5], 2,
-                           level_root=True, root_residual=residual_root)
+
+        self.level2 = Tree(levels[2], block, channels[1], channels[2], 2, level_root=False,root_residual=residual_root)
+        self.level3 = Tree(levels[3], block, channels[2], channels[3], 2, level_root=True, root_residual=residual_root)
+        self.level4 = Tree(levels[4], block, channels[3], channels[4], 2, level_root=True, root_residual=residual_root)
+        self.level5 = Tree(levels[5], block, channels[4], channels[5], 2, level_root=True, root_residual=residual_root)
 
         # for m in self.modules():
         #     if isinstance(m, nn.Conv2d):
@@ -307,6 +309,7 @@ class DLA(nn.Module):
 
 
 def dla34(pretrained=True, **kwargs):  # DLA-34
+    print("run dla34 function")
     model = DLA([1, 1, 1, 2, 2, 1],
                 [16, 32, 64, 128, 256, 512],
                 block=BasicBlock, **kwargs)
@@ -484,7 +487,7 @@ class DLASeg(nn.Module):
 class DLASeg_BIFCN(nn.Module):
     def __init__(self, base_name, heads, pretrained, down_ratio, final_kernel,
                  last_level, head_conv, out_channel=0):
-        super(DLASeg, self).__init__()
+        super(DLASeg_BIFCN, self).__init__()
         assert down_ratio in [2, 4, 8, 16]
         self.first_level = int(np.log2(down_ratio))
         self.last_level = last_level
