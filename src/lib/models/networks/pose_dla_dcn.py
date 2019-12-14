a483db2e56e176024e283eadb419774843f5b7c5
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-14 18:01:20 +0700

    fix

--- src/lib/models/networks/pose_dla_dcn.py
+++ src/lib/models/networks/pose_dla_dcn.py
@@ -502,7 +502,7 @@ class DLASeg_BIFCN(nn.Module):
         print('================')
         
         self.base = globals()[base_name](pretrained=pretrained)
-        self.base2 = nn.Sequential(EfficientNet.from_pretrained('efficientnet-b0'))
+        self.base2 = EfficientNet.from_pretrained('efficientnet-b0')
 
         # print(globals()[base_name])
         # exit(-1)
