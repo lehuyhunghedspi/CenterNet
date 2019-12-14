e00616424b8fdc77942884b55d8b7c1e0e7fa171
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-14 17:58:08 +0700

    fix

--- src/lib/models/networks/efficientnet_pytorch/utils.py
+++ src/lib/models/networks/efficientnet_pytorch/utils.py
@@ -330,7 +330,33 @@ def load_pretrained_weights(model, model_name, load_fc=True):
             else:
                 state_dict_new['base2.0.'+key]=value
         from torchsummary import summary
-        print(summary(model))
+        def torch_summarize(model, show_weights=True, show_parameters=True):
+            """Summarizes torch model by showing trainable parameters and weights."""
+            tmpstr = model.__class__.__name__ + ' (\n'
+            for key, module in model._modules.items():
+                # if it contains layers let call it recursively to get params and weights
+                if type(module) in [
+                    torch.nn.modules.container.Container,
+                    torch.nn.modules.container.Sequential
+                ]:
+                    modstr = torch_summarize(module)
+                else:
+                    modstr = module.__repr__()
+                modstr = _addindent(modstr, 2)
+
+                params = sum([np.prod(p.size()) for p in module.parameters()])
+                weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+                tmpstr += '  (' + key + '): ' + modstr 
+                if show_weights:
+                    tmpstr += ', weights={}'.format(weights)
+                if show_parameters:
+                    tmpstr +=  ', parameters={}'.format(params)
+                tmpstr += '\n'   
+
+            tmpstr = tmpstr + ')'
+            return tmpstr
+        print(torch_summarize(model))
         model.load_state_dict(state_dict)
     else:
         print("not use load fc!!!!!!!")
