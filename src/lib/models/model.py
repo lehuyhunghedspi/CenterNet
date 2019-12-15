8a01254a91a3c183df502cd41bfc559d22566d6a
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-15 22:00:24 +0700

    fix

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -73,7 +73,7 @@ def load_model(model, model_path, optimizer=None, resume=False,
 
           print("use load fc========")
           for key, value in state_dict_base.items(): 
-              print(key)
+              # print(key)
               state_dict['base2.'+key].copy_(value)
 
           
