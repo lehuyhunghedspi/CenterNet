32d71cc2171d2f39957271a43e68bbe0b3b8446e
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-15 22:48:42 +0700

    fix

--- src/lib/models/networks/pose_dla_dcn.py
+++ src/lib/models/networks/pose_dla_dcn.py
@@ -542,8 +542,9 @@ class DLASeg_BIFCN(nn.Module):
                 fill_fc_weights(fc)
             self.__setattr__(head, fc)
 
+        evice = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.adjust_conv1_1=[nn.Conv2d(in_layer_count, out_layer_count, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)) for (in_layer_count,out_layer_count) in [(3,16),(24,32),(32,64),(54,128),(160,256),(448,512)]]
+        self.adjust_conv1_1=[nn.Conv2d(in_layer_count, out_layer_count, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), device=device) for (in_layer_count,out_layer_count) in [(3,16),(24,32),(32,64),(54,128),(160,256),(448,512)]]
 
 
 
